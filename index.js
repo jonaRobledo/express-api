80db1a88598e73ba5cabaeb56e6384fede7feb03
Jona Robledo <jonarobledo.07.jr@outlook.com>
2024-01-25 20:58:51 -0300

    Added 'POST' method for creating a new movie

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+// Imports Dependencies
 const express = require('express')
+const morgan = require('morgan')
+const z = require('zod')
 
+// Imports Core Modules
+const crypto = require('crypto')
+
+// Imports Project Modules
 const movies = require('./movies.json')
+const { validateMovie } = require('./schemas/movies')
 
 const app = express()
 
@@ -8,11 +16,26 @@ const PORT = process.env.PORT ?? 3000
 
 app.disable('x-powered-by') // Desabilita el header X-Powered-By: Express
 
+app.use(morgan('tiny'))
+app.use(express.json())
+
 app.get('/', (req, res) => {
 	res.json({ message: 'Hello World!' })
 })
 
+// GET Movies Routes
 app.get('/movies', (req, res) => {
+	const { genre } = req.query
+	if (genre) {
+		const filteredMovies = movies.filter((movie) =>
+			movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+		)
+		return filteredMovies
+			? res.json(filteredMovies)
+			: res.json({ message: 'No movies of this genre were found' })
+	} else {
+		return res.json({ message: 'Genre vas not indicated', movies })
+	}
 	res.json(movies)
 })
 
@@ -23,6 +46,28 @@ app.get('/movies/:id', (req, res) => {
 		? res.json(movie)
 		: res.status(404).json({ message: 'Movie not Found' })
 })
+
+// POST Movies Routes
+app.post('/movies', (req, res) => {
+	const result = validateMovie(req.body)
+
+	if (result.error)
+		return res.status(400).json({ error: JSON.parse(result.error.message) })
+
+	const newMovie = {
+		id: crypto.randomUUID(), // Generate a random UUID v4
+		...result.data
+	}
+
+	// No es REST porque se rompe el principio de Stateless
+	movies.push(newMovie)
+
+	res.status(201).json(movies)
+})
+
+// PATCH Movies Routes
+// PUT Movies Routes
+// DELETE Movies Routes
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port http://localhost:${PORT}`)
